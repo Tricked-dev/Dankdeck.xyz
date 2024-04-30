883b0baf9960cd69d797014b82e1e3daad893ece
Tricked <72335827+Tricked-dev@users.noreply.github.com>
2024-04-30 09:21:58 +0000

    create main page

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -1,49 +1,3 @@
-<script lang="ts">
-  import type { getSession } from "auth-astro/server";
-  let { session }: { session: Awaited<ReturnType<typeof getSession>> } =
-    $props();
-</script>
-
-<div class="navbar bg-base-100">
-  <div class="flex-1">
-    <a class="btn btn-ghost text-xl">Card Thingy</a>
-  </div>
-  <div class="flex-none gap-2">
-    <div class="p-2 bg-base-200 rounded-2xl w-20">
-      <span class="text-primary">$</span> 20
-    </div>
-
-    <div class="form-control">
-      <button class="btn"> Roll new card </button>
-    </div>
-
-    <div class="dropdown dropdown-end">
-      <div tabindex="0" role="button" class="btn btn-ghost btn-circle avatar">
-        <div class="w-10 rounded-full">
-          <img alt="Tailwind CSS Navbar component" src={session?.user?.image} />
-        </div>
-      </div>
-      <ul
-        tabindex="0"
-        class="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52"
-      >
-        <li>
-          <a class="justify-between">
-            Profile
-            <span class="badge">New</span>
-          </a>
-        </li>
-        <li><a>Settings</a></li>
-        <li><a>Logout</a></li>
-      </ul>
-    </div>
-  </div>
-</div>
-
-<!-- <CodeSnippet></CodeSnippet> -->
-
-<!-- <div class="mx-auto py-7"></div> -->
-
 <div class="w-full">
   <span class="text-7xl flex p-3 font-serif font-bold"
     ><span class="mx-auto">Marketplace</span></span
@@ -53,7 +7,9 @@
   </div>
   <div class="flex flex-wrap justify-center w-full max-w-[70rem] gap-2 mx-auto">
     {#each new Array(20) as _, index}
-      <div class="bg-base-200 w-64 h-96 rounded-lg relative">
+      <div
+        class="bg-base-200 w-64 h-96 rounded-lg relative hover:scale-105 duration-200 shadow-sm hover:shadow-accent hover:z-40"
+      >
         <div class="absolute top-0 left-0 text-xl font-bold p-2">
           ${(index % 8) + 12}
         </div>
---
+++ src/components/Header.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+  import type { getSession } from "auth-astro/server";
+  let { session }: { session: Awaited<ReturnType<typeof getSession>> } =
+    $props();
+</script>
+
+<div class="navbar bg-base-300">
+  <div class="flex-1">
+    <a class="btn btn-ghost text-xl">Card Thingy</a>
+  </div>
+  <div class="flex-none gap-2">
+    <div class="p-2 bg-base-200 rounded-2xl w-20">
+      <span class="text-primary">$</span> 20
+    </div>
+
+    <div class="form-control">
+      <button class="btn"> Roll new card </button>
+    </div>
+
+    <div class="dropdown dropdown-end">
+      <div tabindex="0" role="button" class="btn btn-ghost btn-circle avatar">
+        <div class="w-10 rounded-full">
+          <img alt="Tailwind CSS Navbar component" src={session?.user?.image} />
+        </div>
+      </div>
+      <ul
+        tabindex="0"
+        class="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52"
+      >
+        <li>
+          <a class="justify-between">
+            Profile
+            <span class="badge">New</span>
+          </a>
+        </li>
+        <li><a>Settings</a></li>
+        <li><a>Logout</a></li>
+      </ul>
+    </div>
+  </div>
+</div>
