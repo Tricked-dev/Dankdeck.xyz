48f42c24f3b55d0a9421c9b0f04ef5b6c05cba3a
Abd <44587812+BrahR@users.noreply.github.com>
2024-05-05 14:30:08 +0400

    Added vite import alias

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import auth from "auth-astro";
 import { defineConfig } from 'astro/config';
+import { fileURLToPath } from "bun";
 import svelte from "@astrojs/svelte";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
@@ -8,5 +9,13 @@ import vercel from "@astrojs/vercel/serverless";
 export default defineConfig({
     output: "server",
     adapter: vercel({}),
-    integrations: [svelte(), tailwind(), auth()]
+    integrations: [svelte(), tailwind(), auth()],
+    vite: {
+        resolve: {
+            alias: {
+                '@': fileURLToPath(new URL('./src', import.meta.url).toString()), // ts likes to cry without .toString() for some reason
+
+            }
+        }
+    }
 });
