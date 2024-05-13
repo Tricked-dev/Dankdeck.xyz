adc6ebc54148c4bb4fcaf283fb4febdd49fa2dbb
Tricked <tricked@tricked.pro>
2024-05-13 21:24:09 +0200

    feat: og image (testing)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import auth from "auth-astro";
 import { defineConfig } from 'astro/config';
 import { fileURLToPath } from "url";
+import react from "@astrojs/react";
 import svelte from "@astrojs/svelte";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
@@ -9,12 +10,11 @@ import vercel from "@astrojs/vercel/serverless";
 export default defineConfig({
     output: "server",
     adapter: vercel({}),
-    integrations: [svelte(), tailwind(), auth()],
+    integrations: [svelte(), tailwind(), auth(), react()],
     vite: {
         resolve: {
             alias: {
-                '@': fileURLToPath(new URL('./src', import.meta.url).toString()), // ts likes to cry without .toString() for some reason
-
+                '@': fileURLToPath(new URL('./src', import.meta.url).toString()) // ts likes to cry without .toString() for some reason
             }
         }
     }
---
+++ src/pages/og/card.png.ts
@@ -0,0 +1,6 @@
+import { ImageResponse } from "@vercel/og";
+import { cardImage } from "@/components/og/card";
+
+export const ALL = () => {
+  return cardImage();
+};
