3a8caaeb31de52eb0afba61ec4cb9fcc1b965891
Tricked <tricked@tricked.pro>
2024-05-25 21:42:01 +0200

    fix: skull countdown and check

--- src/components/profile/SkullSettings.svelte
+++ src/components/profile/SkullSettings.svelte
@@ -8,12 +8,16 @@
     user: User;
   }
   let { user: usr }: Props = $props();
+  let seconds = 15 * 100;
   let user = $state(usr);
-  let hoverTimeout = $state(1500);
+  let hoverTimeout = $state(seconds);
   let hovering = $state(false);
 
   onMount(() => {
     let timeout = setInterval(() => {
+      if (!hovering && hoverTimeout < seconds) {
+        hoverTimeout += 1;
+      }
       if (hovering && hoverTimeout > 0) {
         hoverTimeout += -1;
       } else {
@@ -24,21 +28,27 @@
   });
 </script>
 
-<div class="flex gap-1 flex-col">
+<div class="flex gap-1 flex-col min-h-96">
   <div class="flex gap-1 flex-col mb-3">
     <div class="text-lg font-bold leading-5 uppercase">Danger zone</div>
     <div class="text-sm">No one should be in this place ☠️.</div>
   </div>
   <div
-    class="flex flex-col gap-3"
+    class="flex flex-row flex-1 gap-3 text-center justify-center"
     onmouseenter={() => (hovering = true)}
     onmouseleave={() => (hovering = false)}
     onclick={() =>
       tr(async () => {
+        if (hoverTimeout !== 0) return;
         await trpc.deleteAccount.mutate();
         await signOut();
       })}
   >
     <BigSkull />
+    {#if hoverTimeout == 0}
+      No confirm account delete on skull click
+    {:else}
+      {(hoverTimeout / 100).toFixed(2)}s
+    {/if}
   </div>
 </div>
