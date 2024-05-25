2e4d1ed0ceca85c4b5bfa544d7ffb61903b5d9ab
Tricked <tricked@tricked.pro>
2024-05-25 18:48:53 +0200

    fix: account deleting not working

--- src/components/profile/SkullSettings.svelte
+++ src/components/profile/SkullSettings.svelte
@@ -3,6 +3,7 @@
   import { signIn, signOut } from "auth-astro/client";
   import { Github, Discord, BigSkull } from "@/components/icons/index";
   import { onMount } from "svelte";
+  import { tr, trpc } from "@/lib/api";
   interface Props {
     user: User;
   }
@@ -23,16 +24,21 @@
   });
 </script>
 
-
 <div class="flex gap-1 flex-col">
   <div class="flex gap-1 flex-col mb-3">
     <div class="text-lg font-bold leading-5 uppercase">Danger zone</div>
     <div class="text-sm">No one should be in this place ☠️.</div>
   </div>
-  <div class="flex flex-col gap-3">
+  <div
+    class="flex flex-col gap-3"
+    onmouseenter={() => (hovering = true)}
+    onmouseleave={() => (hovering = false)}
+    onclick={() =>
+      tr(async () => {
+        await trpc.deleteAccount.mutate();
+        await signOut();
+      })}
+  >
     <BigSkull />
   </div>
 </div>
-
-
-
